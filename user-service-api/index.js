f8f15122fad8cb5beb6e34ff0d890423372ccc8f
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-06-26 15:30:37 +0700

    upload simple express

---
+++ user-service-api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>
+  res.json([
+    {
+      name: "TruongTV",
+      email: "TruongTV1399it@gmail.com",
+    },
+  ])
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
